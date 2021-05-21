5fed5a71443b1943ab14775ff1e97f940f500573
Mangor1no <tungtm998@gmail.com>
2021-05-22 02:34:47 +0700

    update eslint

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import {
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer,
+  IfFirebaseAuthed,
+  IfFirebaseAuthedAnd,
+} from '@react-firebase/auth';
 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
@@ -11,26 +18,24 @@ import rootReducer from './data/reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(rootReducer, composeWithDevTools());
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyBssaGeetvO1GW7fEjF7VEj3mmY6EmTwNI',
   authDomain: 'todosaga-c25bd.firebaseapp.com',
+  databaseURL: 'https://todosaga-c25bd-default-rtdb.firebaseio.com',
   projectId: 'todosaga-c25bd',
   storageBucket: 'todosaga-c25bd.appspot.com',
   messagingSenderId: '480026199700',
   appId: '1:480026199700:web:d61578afc67b65017a1681',
-  databaseURL: 'https://todosaga-c25bd-default-rtdb.firebaseio.com',
   measurementId: 'G-MRSWGVXP5S',
 };
 
-// Use your config values here.
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <FirebaseAuthProvider {...firebaseConfig} firebase={firebase}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </FirebaseAuthProvider>,
   document.getElementById('root')
 );
 
