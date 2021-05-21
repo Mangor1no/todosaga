571a2c1ef1a88de67965b483c13fb00a2686d4e5
Mangor1no <tungtm998@gmail.com>
2021-05-22 02:18:51 +0700

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -56,7 +56,6 @@ module.exports = {
       },
     ],
     'no-unused-vars': 'warn',
-    '@typescript-eslint/no-unused-vars': ['warn'],
     'no-tabs': 'error',
     'max-lines': 'warn',
     'symbol-description': 'off',
